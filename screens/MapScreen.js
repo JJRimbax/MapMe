036be6bb598d4fd7ed1641843a8b02c3876f5413
Jules Raimbaud <jules.raimbaud@gmail.com>
2024-09-21 00:08:14 +0200

    added refresh target

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,3 +1,4 @@
+import { Button, Alert } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import MapView, { Polyline, Marker } from 'react-native-maps';
@@ -12,13 +13,11 @@ export default function MapScreen() {
     let subscriber = null;
 
     const getLocationUpdates = async () => {
-
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         console.log('Permission de localisation non accordée');
         return;
       }
-
 
       let location = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
       const { latitude, longitude, heading } = location.coords;
@@ -31,15 +30,13 @@ export default function MapScreen() {
       setRouteCoordinates(prevCoords => [...prevCoords, { latitude, longitude }]);
       setHeading(heading);
 
-
       subscriber = await Location.watchPositionAsync(
         {
           accuracy: Location.Accuracy.High,
-          distanceInterval: 5, 
+          distanceInterval: 5, // Met à jour tous les 5 mètres
         },
         (location) => {
           const { latitude, longitude, heading } = location.coords;
-
           setPosition({
             ...position,
             latitude,
@@ -61,30 +58,44 @@ export default function MapScreen() {
   }, []);
 
   if (!position) {
-    return null; 
+    return null;
   }
+
+  const sendSOSAlert = () => {
+    Alert.alert("SOS", "Alerte envoyée avec votre position actuelle !");
+    // Logique pour envoyer la localisation via un SMS ou une API tierce
+    // Par exemple, tu peux utiliser un service d'envoi d'email ou d'API de SMS
+  };
 
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
-        region={position}
+        region={{
+          latitude: position.latitude,
+          longitude: position.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
         showsUserLocation={false}
         followsUserLocation={false}
       >
-
         <Polyline coordinates={routeCoordinates} strokeWidth={5} strokeColor="blue" />
-
         <Marker
           coordinate={position}
           anchor={{ x: 0.5, y: 0.5 }}
           rotation={heading}
           flat={true}
         >
-  
           <View style={styles.marker} />
         </Marker>
       </MapView>
+      {/* Ajout du bouton SOS en overlay sur la carte */}
+      <Button
+        title="SOS"
+        color="red"
+        onPress={sendSOSAlert}
+      />
     </View>
   );
 }
